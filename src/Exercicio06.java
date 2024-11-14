977a25954839400d5aeeceef8760a4acf9172ab7
LizReis <124801714+LizReis@users.noreply.github.com>
2024-11-14 19:56:36 -0300

    Exercicio06 atualizado

--- src/Exercicio06.java
+++ src/Exercicio06.java
@@ -9,5 +9,7 @@ public class Exercicio06 {
             anterior = atual;
             atual = proximo;
         }
+        //Faz a última soma que não aparece apois ele chegar em 100, nesse caso, o numero anterior foi o 55 + 89 (valor atual), que dará 144
+        System.out.println(atual + anterior);
     }
 }
